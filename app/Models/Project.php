6520c4bc2adfbc6434668564a453c6bf15b42fbd
Marta Scardillo <martascardillo02@gmail.com>
2024-01-10 14:39:39 +0100

    Add Technology migration, model and belongsToMany

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -20,4 +20,12 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
-}
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
+};
+
+
+
+
